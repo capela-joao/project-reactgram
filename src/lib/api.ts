b78d17388658fce781c5b6faa0de13d20e6fb1b1
capela-joao <joaopaulo1060@hotmail.com>
2026-01-06 15:18:10 -0300

    Ajuste na chamada da variável de ambiente no requestConfig.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export const api = `${API_BASE_URL}/api`;
 export const uploads = `${API_BASE_URL}/uploads`;
 
