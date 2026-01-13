5acef9177f6b6523961b96d0660725c09fdd3ac0
capela-joao <joaopaulo1060@hotmail.com>
2026-01-13 19:34:11 -0300

    Fix: Atualizando URL da imagem de perfil exibida no feed.

--- src/components/User/DashboardProfile.tsx
+++ src/components/User/DashboardProfile.tsx
@@ -10,7 +10,7 @@ const DashboardProfile = ({ profile }: dashboardProfileProps) => {
     <div>
       <div className="flex gap-2 items-center">
         <img
-          src={`${process.env.NEXT_PUBLIC_API_UPLOADS_URL}/users/${profile.profileImage}`}
+          src={profile.profileImage}
           alt={profile.username}
           className="w-12 h-12 rounded-full"
         />
